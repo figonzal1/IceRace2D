76749fa6f8c9160b5a65ec7e7ba825ef14f9af7e
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-06-13 19:02:00 -0400

    update to new unity LTS 2022 version

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -14,20 +14,20 @@ public class CarController : MonoBehaviour
 	[SerializeField]
 	private Rigidbody2D backWheel;
 
-	public float torque = 10f;
-	public float speed = 100;
+	public float torque;
+	public float maxSpeed;
 
 	private float movement;
 
 	void Update()
 	{
-		movement = Input.GetAxis("Vertical");
+		movement = Input.GetAxisRaw("Vertical");
 	}
 
 	private void FixedUpdate()
 	{
 		carRigid.AddTorque(movement * torque * Time.fixedDeltaTime);
-		frontWheel.AddTorque(-movement * speed * Time.fixedDeltaTime);
-		backWheel.AddTorque(-movement * speed * Time.fixedDeltaTime);
+		frontWheel.AddTorque(-movement * maxSpeed * Time.fixedDeltaTime);
+		backWheel.AddTorque(-movement * maxSpeed * Time.fixedDeltaTime);
 	}
 }
